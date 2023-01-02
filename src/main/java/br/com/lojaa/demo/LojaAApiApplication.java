20e1bc215913166e01c409d90ada9fefe5dda245
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-01-02 20:10:39 -0300

    first commit

---
+++ src/main/java/br/com/lojaa/demo/LojaAApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojaa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaAApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaAApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lojaa/demo/LojaAApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojaa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaAApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
